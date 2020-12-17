cc3718a77dbee364eaf83289ba6a57fa08e37894
Alex Racinski <aracinski2@gmail.com>
2020-12-16 20:43:23 -0600

    add index

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for='product in products'>
+      <h3>{{ product.name }}</h3>
+      <h5>${{ product.price }}</h5>
+      <h5>{{ product.description }}</h5><br>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Welcome to my store!",
+      products: [],
+    };
+  },
+  created: function () {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("ooo shiny products");
+      axios.get("/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
+};
+</script>
